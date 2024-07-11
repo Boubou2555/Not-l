6807f4635f1c21477e4362bd351382b584143974
Boubou2555 <147002515+Boubou2555@users.noreply.github.com>
2024-07-11 02:37:15 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -51,16 +51,6 @@ const App = () => {
             <div className="bg-[#1f1f1f] text-center py-2 rounded-xl">
               <p className="text-lg">borhane hex devlopment 😏 <Arrow size={18} className="ml-0 mb-1 inline-block" /></p>
               <p className="text-lg"> ❤️ صلي على النبي ❤️ <Arrow size={20} className="ml-0 mb-1 inline-block" /></p>
-            </div>
-            <div className="w-full z-10 min-h-screen flex flex-col items-center text-white">
-
-        <div className="fixed top-2 left-0 w-full px-6 pt-7 z-11 flex flex-col items-center text-white">
-          <div className="w-full cursor-pointer">
-            <div className="bg-[#1f1f1f] text-center py-2 rounded-xl">
-              <p className="text-lg">borhane hex devlopment 😏 <Arrow size={18} className="ml-0 mb-1 inline-block" /></p>
-              <p className="text-lg">❤️ صلي على النبي ❤️ <Arrow size={20} className="ml-0 mb-1 inline-block" /></p>
-            </div>
-            
           </div>
           <div className="mt-12 text-5xl font-bold flex items-center">
             <img src={coin} width={44} height={44} />
