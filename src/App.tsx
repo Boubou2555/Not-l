6d1942e672609432bbcdb99fc345273aa787ea1d
Boubou2555 <147002515+Boubou2555@users.noreply.github.com>
2024-07-11 02:04:15 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -75,7 +75,7 @@ const App = () => {
               </div>
             </div>
             <div className="flex-grow flex items-center max-w-60 text-sm">
-              <div className="w-full bg-[#e4be4e] py-4 rounded-2xl flex justify-around">
+              <div className="w-full bg-[#325a6b] py-4 rounded-2xl flex justify-around">
                 <button className="flex flex-col items-center gap-1">
                   <img src={bear} width={24} height={24} alt="High Voltage" />
                   <span>borhane</span>
